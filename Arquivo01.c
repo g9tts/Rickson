f497345f60664568c5e16e2d98e57411e2c682a1
g9tts <55061534+g9tts@users.noreply.github.com>
2019-09-28 15:59:03 -0300

    Update Arquivo01.c

--- Arquivo01.c
+++ Arquivo01.c
@@ -2,176 +2,191 @@
 #include <stdlib.h>
 #include <locale.h> 
 #include <math.h>
-#include <conio.h>
- 
- int main(){               //Questao 1
- 	//Adaptação do programa para PT-BR
- 	
-	 setlocale(LC_ALL, "Portuguese");
- 	
-	 //Definição de variavel
- 	
-	 int a, num, in, c, x, y, k, pot, fat, n, resultado = 0; 
-	 float cd1, cd2, cd3, cr, media, b, n1 = 1, n2 = 2, mp;
-	 long decimal, q, r;
-	 int i, j = 0;
-	 char hexadecimal[100];
-	 char nome[45];
-	 double w = 10;
-	 double h = 10;
-	 double logaritmo_natural = 0;
-	 double logaritmo_xbase10 = 0;
- 	
-	 //Informações sobre selecionaveis + leitor de variavel
- 	 system("cls");
-	 printf("\nDigite 0 para finalizar o programa\n"
-	           "1 para o menu um\n"
-		   "2 para o menu dois\n"
-		   "3 para o menu três\n"
-		   "4 para o menu quatro\n"
-		   "5 para o menu cinco\n"
-		   "6 para o menu seis\n"
-		   "7 para o menu sete\n"
-		   "8 para o menu oito\n"
-		   "9 para o menu nove\n"
-		   "10 para o menu dez\n");
-	scanf("%d", &a); 
-	system("cls");
+#include <conio.h> 
+
+int main (){
 	
-	   //Estrutura a ser repitida
+	//Questao 1
+ 	
+	int a=1, num, in, c, x, y, k, pot, fat, n, resultado = 0;  //Definição de variavel
+	float cd1, cd2, cd3, cr, media, b, n1 = 1, n2 = 2, mp;
+	long decimal, q, r;
+	int i, j = 0;
+	char hexadecimal[100];
+	char nome[45];
+	double w = 10;
+	double h = 10;
+	double logaritmo_natural = 0;
+	double logaritmo_xbase10 = 0;
 	
-	while(a)
-	{
-	   //Condicionais
-	   
-	   if(a == 1){                            //Questao 2
-	   printf("Bem-vindo ao menu 1\n");  
-	   printf("digite um numero inteiro: ");     //Indentifica se o numero é par ou impar
-		scanf("%d",&num);
-		if(num%2==0)//operador %
-		{
-		printf("Par");
-		}
-		else{
-		printf("Ímpar");
-		}
-       }
-	   else if(a == 2){                       //Questao 3
-	   printf("Bem-vindo ao menu 2\n");         //Potenciação
-	   printf ("Primeiro numero: ");
-     	scanf ("%d",&x);
-     	printf ("Segundo numero: ");
-     	scanf ("%d",&y);
+	while (a!=0){
+	
+	setlocale(LC_ALL, "Portuguese"); //Adaptação do programa para PT-BR
+	printf("\nDigite de 1 a 10 pra escolher um menu: \n");
+	scanf("%d", &a);
+	
+
+	
+	switch (a){
+		
+		case 1: {
+			system("cls");
+			printf("Bem-vindo ao menu 1\n");
+		
+		    printf("digite um numero inteiro: ");     //Indentifica se o numero é par ou impar
+			scanf("%d",&num);
+			if(num%2==0)//operador %
+			{
+			printf("Par");
+			}
+			else{
+			printf("Ímpar");
+			}
+			break;
+				}
+		
+		case 2: {
+			system("cls");
+			printf("Bem-vindo ao menu 2\n");      //Questao 3
+			
+			printf ("Digite o primeiro numero: "); //Potenciação
+     		scanf ("%d",&x);
+     		printf ("Digite o segundo numero: ");
+     		scanf ("%d",&y);
  
-     	for (k>0;k<y;k++)
-     	{
-     	pot = x*(x*k);
-     	}
+     		for (k>0;k<y;k++)
+     		{
+     		pot = x*(x*k);
+     		}
     	 
-        printf ("\n O resuldo é : %d", pot);
-	   }
-	   else if(a == 3){               
-	   printf("Bem-vindo ao menu 3\n");      //Logaritmo     //Questao 4
-	   printf("Digite suas 2 numeros para operação logaritmo: \n");
-	   scanf("%f %f", &w, &h);  
-	   logaritmo_natural = log(w);
+        	printf ("\n O resuldo é : %d", pot);
+			break; 
+				}
+		
+		case 3: {                            //Logaritmo     //Questao 4
+			system("cls"); 
+			printf("Bem-vindo ao menu 3\n");
+			
+			printf("Digite suas 2 numeros para operação logaritmo: \n");
+	   		scanf("%f %f", &w, &h);  
+	   		logaritmo_natural = log(w);
 	   
-	   printf("Logaritmo natural de x %.2f = %.2f\n",w, logaritmo_natural);
+	   		printf("Logaritmo natural de x %.2f = %.2f\n",w, logaritmo_natural);
 	
-	   logaritmo_xbase10 = log10(w);
-	   printf("Logaritmo de x na base10 %.2f = %.2f\n",w, logaritmo_xbase10);
+	   		logaritmo_xbase10 = log10(w);
+	   		printf("Logaritmo de x na base10 %.2f = %.2f\n",w, logaritmo_xbase10);
 	   
-	   logaritmo_natural = log(h);
+	   		logaritmo_natural = log(h);
 	   
-	   printf("Logaritmo natural de y %.2f = %.2f\n",h, logaritmo_natural);
+	   		printf("Logaritmo natural de y %.2f = %.2f\n",h, logaritmo_natural);
 	   
-	   logaritmo_xbase10 = log10(h);
-	   printf("Logaritmo de y na base10 %.2f = %.2f\n",h, logaritmo_xbase10);
-	   
-       }
-	   else if(a == 4){                   //Questao 5
-	   printf("Bem-vindo ao menu 4\n");   //Indentifica se o ano é bissexto ou nao
-	  printf("digite um ano: ");
-		scanf("%d",&num);
-		if(num%4==0)   //operador %
-		{
-		printf("Ano Bissexto");
-		}
-		else{
-		printf("Ano nao Bissexto");
-		}
-       }
-	   else if(a == 5){                    //Questao 6
-	   printf("Bem-vindo ao menu 5\n");       
-	  printf("Digite suas 3 notas, para o calculo da media\n");
-	   scanf("%f %f %f", &cd1, &cd2, &cd3);
+	   		logaritmo_xbase10 = log10(h);
+	   		printf("Logaritmo de y na base10 %.2f = %.2f\n",h, logaritmo_xbase10);
+			break; 
+				}
+		
+		case 4: {
+			system("cls");
+			printf("Bem-vindo ao menu 4\n");   //Questao 5
+			
+			printf("digite um ano: ");      //Indentifica se o ano é bissexto ou nao
+			scanf("%d",&num);
+			if(num%4==0)   //operador %
+			{
+			printf("Ano Bissexto");
+			}
+			else{
+			printf("Ano nao Bissexto");
+			}
+			
+			break;
+				}
+	
+		case 5: {                                //Questao 6
+			system("cls");
+			printf("Bem-vindo ao menu 5\n");
+			
+			printf("Digite suas 3 notas, para o calculo da media\n");
+	   		scanf("%f %f %f", &cd1, &cd2, &cd3);
 	   
 	         cr=(cd1*n1)+(cd2*n1)+(cd3*n2)/cd1+cd2+cd3; //Calculo do CRE
 	         media = (cd1 + cd2 + cd3)/3;               //Calculo da media
 	         mp=(cd1*n1)+(cd2*n1)+(cd3*n2)/1+2+3;             //Calculo da media ponderada
 	   
-	   printf("Sua media é %f\n", media);
-	   printf("Media pondera é %f\n", mp);
-	   printf("Seu CRE é %f\n", cr);
-	   if(media>=6.0){
-	   	printf("Parabens, voce foi aprovado!");
-	   }
-	   else
-	   printf("Infelizmente, voce foi reprovado!");
-       }
-	   else if(a == 6){                      //Questao 7
-	   printf("Bem-vindo ao menu 6\n");
-	     printf("Digite suas 2 notas, para o calculo da media\n"); //Calculo da media
-	   scanf("%f %f", &cd1, &cd2);  
-	   media = (cd1 + cd2)/2;
+	   		printf("Sua media é %f\n", media);
+	   		printf("Media pondera é %f\n", mp);
+	   		printf("Seu CRE é %f\n", cr);
+	   		if(media>=6.0){
+	   		printf("Parabens, voce foi aprovado!");
+	   		}
+	   		else{
+	   		printf("Infelizmente, voce foi reprovado!");
+       		}
+			break;
+				}
+		
+		case 6: {                                         //Questao 7
+			system("cls"); 
+			printf("Bem-vindo ao menu 6\n");
+			
+			printf("Digite suas 2 notas, para o calculo da media\n"); //Calculo da media
+	   		scanf("%f %f", &cd1, &cd2);  
+	   		media = (cd1 + cd2)/2;
 	   
-	   if(cd1>0 && cd1<=10 && cd2>0 && cd2<=10){         //Notas apenas entre 0 e 10
-	   	printf("Sua media é %f", media);
-       }
-       else{
-       	printf("ERROR404\n");
-	   } 
-        
-       }
-	   else if(a == 7){
-	   printf("Bem-vindo ao menu 7\n"); 
-	   printf("Insira um valor para o qual deseja calcular seu fatorial: ");
-       scanf("%d", &n);
+	   		if(cd1>0 && cd1<=10 && cd2>0 && cd2<=10){         //Notas apenas entre 0 e 10
+	   		printf("Sua media é %f", media);
+      		}
+       		else{
+       		printf("ERROR404\n");
+	   		}
+			break;
+				}
+			
+		case 7:{                                     // Calculo do valor fatorial
+			system("cls");
+			printf("Bem-vindo ao menu 7\n");
+			
+			printf("Insira um valor para o qual deseja calcular seu fatorial: ");
+       		scanf("%d", &n);
  
-       for(fat = 1; n > 1; n = n - 1);
-       fat = fat * n;
+       		for(fat = 1; n > 1; n = n - 1);
+       		fat = fat * n;
  
-       printf("\nFatorial calculado: %d", fat); 
-  	   }
-	   else if(a == 8){
-	   printf("Bem-vindo ao menu 8\n"); 
-	   printf("Digite um número: ");
- 	   scanf("%d", &num);
+       		printf("\nFatorial calculado: %d", fat);
+			break;
+				}
+		
+		case 8: {                       //Calcula se o numero é primo ou não
+			system("cls");
+			printf("Bem-vindo ao menu 8\n");
+			
+			printf("Digite um número: ");
+ 	   		scanf("%d", &num);
  
- 	for (i = 2; i <= num / 2; i++) {
-    if (num % i == 0) {
-       resultado++;
-       break;
-    }
-    }
- 
-    if (resultado == 0)
-    printf("%d é um número primo\n", num);
-    else
-    printf("%d não é um número primo\n", num);
-	   }
-	   else if(a == 9){
-	   printf("Bem-vindo ao menu 9\n");
-	   }
-	   else if(a == 10){                    //Questao 8
-	   printf("Bem-vindo ao menu 10\n"); //Transformação da matricula em hexa-decimal e a repitação do nome
-       printf("sua matricula, por favor: \n");
+ 			for (i = 2; i <= num / 2; i++) {
+    		if (num % i == 0) {
+       		resultado++;
+       		break;
+    		}
+			break;
+				}
+			
+		case 9:{
+			system("cls");
+			printf("Bem-vindo ao menu 9\n");
+			break;
+				}
+		
+		case 10: {                  //Questao 8
+			system("cls");   
+			printf("Bem-vindo ao menu 10\n");
+			printf("sua matricula, por favor: \n");  
 	   scanf("%ld", &decimal);
 	   printf("seu nome, por favor: ");
 	   scanf("%s",nome);
 	 
 	   q = decimal;
-	 
+	                                               //Transformação da matricula em hexa-decimal e a repitação do nome
 	   while (q != 0)
 	   {
 	   r = q % 16;          
@@ -189,32 +204,26 @@
 	  for (i = j; i >= 0; i--)
 	  printf("%c", hexadecimal[i]);
 	  printf("\n");
-	 }
-	   else{ 
-	   printf("\nComando inválido, por favor digite outro número: ");
-       } 
-    //Opções a serem repetidas + Leitor de variavel
-    
-	printf("\nDigite 0 para finalizar o programa\n"
-	           "1 para o menu um\n"
-		   "2 para o menu dois\n"
-		   "3 para o menu três\n"
-		   "4 para o menu quatro\n"
-		   "5 para o menu cinco\n"
-		   "6 para o menu seis\n"
-		   "7 para o menu sete\n"
-		   "8 para o menu oito\n"
-		   "9 para o menu nove\n"
-		   "10 para o menu dez\n");
-	scanf("%d", &a); 
-	system("cls");
+			break; 
+				}
+		
+		default: 
+		{
+			system("cls");
+			
+			if(a==0){
+				
+				continue;
+			}
+			printf("\n\n Essa opção não existe");
+			break;
+		}
+			}
 	
-	   
-
-	  if (a==0){  //Finaliza o programa
-	printf("Programa finalizado!");
-    }
+			
+	}
+		if (a == 0)          //Finaliza o programa
+		printf("\n\n O programa foi finalizado");
+		getch();
+	}
 }
-    system("cls");
- 	return 0;
- }
